c5f4c5db0d8314c860027c6c047772d1565f8495
smilevictory <leevictory7272@gmail.com>
2025-04-08 14:36:56 +0900

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>푸터</div>
+</template>
+
+<style scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      // 지연로딩 적용!
+      component: () => import('@/pages/Home.vue'),
+    },
+  ],
+})
+export default router
