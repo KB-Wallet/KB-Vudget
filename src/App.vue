4253e3005e5a19c7047fd2e98e67c2a60038e569
star075 <starr075@gmail.com>
2025-04-09 17:10:34 +0900

    [feat] QuickPost 오류 수정

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
-<script setup></script>
+<script setup>
+import Header from './components/HeaderFooter/Header.vue'
+import Footer from './components/HeaderFooter/Footer.vue'
+</script>
 
 <template>
   <div id="app">
+    <Header></Header>
     <router-view />
+    <Footer></Footer>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#app {
+  padding: 30px 50px;
+}
+</style>
---
+++ src/components/HeaderFooter/Footer.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="footer-container">
+<p></p>
+  </div>
+</template>
+
+<style scoped></style>
