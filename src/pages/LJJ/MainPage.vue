b801a6e0a1551db19bedee3532db729b9d0b4d38
Jaejeong-Lee <leejae938@gmail.com>
2025-04-11 10:48:27 +0900

    [feat] 사소한 수정

--- src/pages/LJJ/MainPage.vue
+++ src/pages/LJJ/MainPage.vue
@@ -9,7 +9,7 @@ import ReportCard from '@/components/ReportCard.vue'
     <!-- 카테고리별 지출 요약 차트 -->
     <CategoryCard />
   </div>
-  <!--  -->
+  <!-- 게이지바 수입지출 요약 차트 -->
   <h6>main_slide_1_2 원본</h6>
   <div><ReportCard /></div>
 </template>
